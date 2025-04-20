c27d71fad13d6da034dfcdfb8d381de253413f9b
Yuvi dew.. <127539111+yuvidew@users.noreply.github.com>
2025-04-20 19:31:29 +0530

    Update ShareModal.tsx

--- app/(root)/documents/_components/ShareModal.tsx
+++ app/(root)/documents/_components/ShareModal.tsx
@@ -47,8 +47,14 @@ export const ShareModal = ({
         setLoading(false);
     }
 
+     const onOpenChange = () => {
+        if (currentUserType == "editor") {
+            setOpen(true)
+        }
+    }
+
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
+        <Dialog open={open} onOpenChange={onOpenChange}>
             <DialogTrigger>
                 <Button className=" gradient-blue flex h-9 gap-1 px-4" disabled={currentUserType !== "editor"}>
                     <Image
